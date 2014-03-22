c2eebe395903473e4b8f49b105826207e366fe5a
Divi <sylvain.lorinet@gmail.com>
2014-03-23 00:09:57 +0100

    Add Stackable object.

---
+++ src/Stackable.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * pthreads extension stub file for code completion purposes
+ *
+ * @author Sylvain Lorinet <sylvain.lorinet@gmail.com>
+ * @version 2.0.2
+ */
+ 
+/**
+ * This connects automatically the reference in the creating context and the reference in the threading context
+ */
+class Stackable extends \Threaded { }
